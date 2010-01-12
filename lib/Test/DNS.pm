e33dc7bed7e00b54cc1059f15220f734404b85e1
Sawyer X <xsawyerx@cpan.org>
2010-01-12 18:56:33 +0200

    version 0.02 done

--- lib/Test/DNS.pm
+++ lib/Test/DNS.pm
@@ -12,7 +12,7 @@ has 'object'      => ( is => 'ro', isa => 'Net::DNS::Resolver', lazy_build => 1 
 has 'follow_cname' => ( is => 'rw', isa => 'Bool', default => 0 );
 has 'warnings'     => ( is => 'rw', isa => 'Bool', default => 1 );
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 my $CLASS = __PACKAGE__;
 
@@ -161,7 +161,7 @@ Test::DNS - Test DNS queries and zone configuration
 
 =head1 VERSION
 
-Version 0.01
+Version 0.02
 
 =head1 SYNOPSIS
 
